a324f16d9f85a7e379d7761bbaff9bb9b8dad4c8
David Burger <burgedav002@gmail.com>
2024-03-18 13:19:57 -0500

    fixes

---
+++ tests/test_module.py
@@ -0,0 +1,15 @@
+import pytest
+from Modules.linuxtoolbox import display_menu
+
+def test_display_menu(capsys):
+    display_menu()
+    captured = capsys.readouterr()
+    assert captured.out == "Welcome to Linux Toolbox Menu:\n" \
+                           "1. Disk Options\n" \
+                           "2. Repair options\n" \
+                           "3. Restart Donna AI\n" \
+                           "4. Install Donna AI [in progress]\n" \
+                           "5. Backup Donna AI\n"
+
+if __name__ == "__main__":
+    pytest.main()
